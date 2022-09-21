2aa96f889212bf76e5b1f5d528f79566e2dcc678
HarshalWarade <92624545+HarshalWarade@users.noreply.github.com>
2022-09-21 23:03:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+let navbarsds = document.getElementById('navbarsds');
 let navbars = document.getElementById('navbars');
+let cards = document.getElementById('cards');
+navbars.style.color = '#9d00ff';
+let cardsds = document.getElementById('cardsds');
+cardsds.style.display = 'none';
 navbars.addEventListener('click', function(){
-    
+    navbarsds.style.display = '';
+    cardsds.style.display = 'none';
+    // cardsds.style.display = 'none';
+    cards.style.color = 'black';
+    navbars.style.color = '#9d00ff';
+});
+cards.addEventListener('click', ()=>{
+    cardsds.style.display = '';
+    navbarsds.style.display = 'none';
+    cards.style.color = '#9d00ff';
+    navbars.style.color = 'black';
 })
